35fadc7c78c1a370ebf85f2b8ffb2dfaa016e3d3
winton <mail@wintoni.us>
2018-11-14 12:33:33 -0500

    Using path instead of cwd

--- test/indexTest.js
+++ test/indexTest.js
@@ -24,7 +24,7 @@ beforeEach(async () => {
 
 async function run() {
   await events.cyclops({
-    argv: ["test/fixture"],
+    argv: ["fixture"],
     composer: starter,
     path: __dirname,
     task: "starter-tasks",
