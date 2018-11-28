3d51aa533e7e97ed7b453045105477030bc43507
winton <mail@wintoni.us>
2018-11-27 23:13:36 -0500

    Upgrade template versions, clean up next template

--- templates/next/next.config.js
+++ templates/next/next.config.js
@@ -1,30 +1,5 @@
-const { EnvironmentPlugin } = require("webpack")
+const withSass = require("@zeit/next-sass")
 
-const {
-  BundleAnalyzerPlugin,
-} = require("webpack-bundle-analyzer")
-
-const { ANALYZE } = process.env
-
-module.exports = {
+module.exports = withSass({
   distDir: "dist",
-  webpack: function(config, { isServer }) {
-    config.plugins.push(
-      new EnvironmentPlugin({
-        // Put env var defaults here
-      })
-    )
-
-    if (ANALYZE) {
-      config.plugins.push(
-        new BundleAnalyzerPlugin({
-          analyzerMode: "server",
-          analyzerPort: isServer ? 8888 : 8889,
-          openAnalyzer: true,
-        })
-      )
-    }
-
-    return config
-  },
-}
+})
--- templates/next/test/IndexPageTest.js
+++ templates/next/test/IndexPageTest.js
@@ -1,4 +1,4 @@
-import { mount } from "@invrs/enzyme"
+import { mount } from "enzyme"
 import React from "react"
 import App from "../pages/IndexPage.js"
 
